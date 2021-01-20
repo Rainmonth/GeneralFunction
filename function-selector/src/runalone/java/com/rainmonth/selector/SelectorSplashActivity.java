016c64657992758f890abc39355edc0abd46a45a
张豪成 <zhanghaocheng@hhdd.com>
2021-01-20 17:17:02 +0800

    init

---
+++ function-selector/src/runalone/java/com/rainmonth/selector/SelectorSplashActivity.java
@@ -0,0 +1,14 @@
+package com.rainmonth.selector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectorSplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selector_splash);
+    }
+}
