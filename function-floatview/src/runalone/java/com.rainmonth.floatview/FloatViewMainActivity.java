27eed70cf9a2508bc69569738a89945af58baa01
张豪成 <zhanghaocheng@hhdd.com>
2021-02-22 13:35:33 +0800

    mod->Demo 修改

--- function-floatview/src/runalone/java/com.rainmonth.floatview/FloatViewMainActivity.java
+++ function-floatview/src/runalone/java/com.rainmonth.floatview/FloatViewMainActivity.java
@@ -1,22 +1,12 @@
 package com.rainmonth.floatview;
 
-import android.Manifest;
-import android.graphics.Color;
-import android.graphics.PixelFormat;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.view.View;
+import android.view.Gravity;
 import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.rainmonth.utils.PermissionUtils;
-import com.rainmonth.utils.ToastUtils;
 import com.rainmonth.utils.log.LogUtils;
 
 public class FloatViewMainActivity extends AppCompatActivity {
@@ -30,25 +20,47 @@ public class FloatViewMainActivity extends AppCompatActivity {
 
         floatViewConfig = new FloatViewConfig();
 
-        TextView tvAdd = findViewById(R.id.tv_add);
+        TextView tvAdd1 = findViewById(R.id.tv_add1);
 
-        tvAdd.setOnClickListener(v -> {
-            handleAddClick();
+        TextView tvAdd2 = findViewById(R.id.tv_add2);
+
+        tvAdd1.setOnClickListener(v -> {
+            handleAdd1Click();
+        });
+        tvAdd2.setOnClickListener(v -> {
+            handleAdd2Click();
         });
     }
 
-    private void handleAddClick() {
-        LogUtils.d("FloatView", "handleAddClick");
+    private void handleAdd1Click() {
+        LogUtils.d("FloatView", "handleAdd1Click");
         FloatViewConfig config = new FloatViewConfig.Builder()
                 .setGlobalFloat(true)
                 .setAutoCompat(true)
                 .setHeight(ViewGroup.LayoutParams.WRAP_CONTENT)
                 .setWidth(ViewGroup.LayoutParams.WRAP_CONTENT)
+                .setGravity(Gravity.BOTTOM | Gravity.END)
                 .build();
         FloatViewManager.get()
                 .with(this)
                 .config(config)
-                .add(FloatViewManager.FLOAT_VIEW_ID_MAIN)
-                .show(FloatViewManager.FLOAT_VIEW_ID_MAIN);
+                .add(FloatViewManager.FLOAT_VIEW_ID_MAIN_BOTTOM_LEFT)
+                .show(FloatViewManager.FLOAT_VIEW_ID_MAIN_BOTTOM_LEFT);
+    }
+
+    private void handleAdd2Click() {
+        LogUtils.d("FloatView", "handleAdd2Click");
+        FloatViewConfig config = new FloatViewConfig.Builder()
+                .setGlobalFloat(true)
+                .setAutoCompat(true)
+                .setHeight(ViewGroup.LayoutParams.WRAP_CONTENT)
+                .setWidth(ViewGroup.LayoutParams.WRAP_CONTENT)
+                .setGravity(Gravity.CENTER)
+                .build();
+        FloatViewManager.get()
+                .with(this)
+                .config(config)
+                .add(FloatViewManager.FLOAT_VIEW_ID_MAIN_CENTER)
+                .show(FloatViewManager.FLOAT_VIEW_ID_MAIN_CENTER);
     }
 }
