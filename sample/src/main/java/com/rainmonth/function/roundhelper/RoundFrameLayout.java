225a5693884380346914cd5281ef802b58c0dd30
Randy <rainmonth@yeah.net>
2021-08-31 15:08:09 +0800

    add->添加圆角边框的示例

---
+++ sample/src/main/java/com/rainmonth/function/roundhelper/RoundFrameLayout.java
@@ -0,0 +1,115 @@
+package com.rainmonth.function.roundhelper;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+/**
+ * @author RandyZhang
+ * @date 2021/4/30 11:57 AM
+ */
+public class RoundFrameLayout extends FrameLayout implements RoundMethodInterface {
+
+    private RoundHelper mHelper;
+
+    public RoundFrameLayout(@NonNull Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public RoundFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        if (mHelper == null) {
+            mHelper = new RoundHelper();
+        }
+        mHelper.init(context, attrs, this);
+//        setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mHelper.onSizeChanged(w, h);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        mHelper.preDraw(canvas);
+        super.draw(canvas);
+        mHelper.drawPath(canvas);
+    }
+
+    @Override
+    public void setRadius(float radiusDp) {
+        mHelper.setRadius(radiusDp);
+    }
+
+    @Override
+    public void setRadius(float radiusTopLeftDp, float radiusTopRightDp, float radiusBottomLeftDp, float radiusBottomRightDp) {
+        mHelper.setRadius(radiusTopLeftDp, radiusTopRightDp, radiusBottomLeftDp, radiusBottomRightDp);
+    }
+
+    @Override
+    public void setRadiusLeft(float radiusDp) {
+        mHelper.setRadiusLeft(radiusDp);
+    }
+
+    @Override
+    public void setRadiusRight(float radiusDp) {
+        mHelper.setRadiusRight(radiusDp);
+    }
+
+    @Override
+    public void setRadiusTop(float radiusDp) {
+        mHelper.setRadiusTop(radiusDp);
+    }
+
+    @Override
+    public void setRadiusBottom(float radiusDp) {
+        mHelper.setRadiusBottom(radiusDp);
+    }
+
+    @Override
+    public void setRadiusTopLeft(float radiusDp) {
+        mHelper.setRadiusTopLeft(radiusDp);
+    }
+
+    @Override
+    public void setRadiusTopRight(float radiusDp) {
+        mHelper.setRadiusTopRight(radiusDp);
+    }
+
+    @Override
+    public void setRadiusBottomLeft(float radiusDp) {
+        mHelper.setRadiusBottomLeft(radiusDp);
+    }
+
+    @Override
+    public void setRadiusBottomRight(float radiusDp) {
+        mHelper.setRadiusBottomRight(radiusDp);
+    }
+
+    @Override
+    public void setStrokeWidth(float widthDp) {
+        mHelper.setStrokeWidth(widthDp);
+    }
+
+    @Override
+    public void setStrokeColor(int color) {
+        mHelper.setStrokeColor(color);
+    }
+
+    @Override
+    public void setStrokeWidthColor(float widthDp, int color) {
+        mHelper.setStrokeWidthColor(widthDp, color);
+    }
+}
