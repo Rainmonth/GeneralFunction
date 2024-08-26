043142154a4c246a3d8b551a017c293a7dedbae5
randy <rainmonth@yeah.net>
2024-08-26 15:05:20 +0800

    add->添加本地媒体加载接口

---
+++ function-selector/src/main/java/com/rainmonth/selector/ILocalMediaLoader.java
@@ -0,0 +1,12 @@
+package com.rainmonth.selector;
+
+/**
+ * 本地文件加载
+ * 根据不通的加载参数，从 {@link android.provider.MediaStore}MediaStore 中获取不通类型的媒体文件，包括图片、音频、视频等，主要通过 ContentResolver 获取
+ *
+ * @author 张豪成
+ * @date 2024/6/4 11:41
+ */
+public interface ILocalMediaLoader {
+
+}
